ab5d9623dec03903ec75e2f01acc95f62eef1e64
XxK3vinxX123 <kevin.espejel.huerta@gmail,com>
2025-03-26 13:58:29 -0600

    Version 3.0: Incorporacion de nuevos proyectos, fractales y menu principal

---
+++ FormsFractales/frmFractal01.cs
@@ -0,0 +1,200 @@
+namespace FormsFractales
+{
+    public partial class frmFractal01 : Form
+    {
+        public frmFractal01()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //MandelbrotSet();
+            //SierpinskiTriangle();
+            //KochSnowflake();
+            DrawSpirograph();
+        }
+
+        private void MandelbrotSet()
+        { 
+            int width = ptbMandelbrot.Width;
+            int heigth = ptbMandelbrot.Height;
+
+            Bitmap bmp = new Bitmap(width, heigth);
+
+            for (int row = 0; row < width; row++)
+            {
+                for (int col = 0; col < heigth; col++)
+                {
+                    double c_re = (col - width / 2) * 4.0 / width;
+                    double c_im = (row - heigth / 2) * 4.0 / heigth;
+
+                    int iteraciones = 0;
+                    double x = 0, y = 0;
+
+                    while (iteraciones < 1000 && ((x * x) + (y * y)) < 4.0)
+                    {
+                        double x_temporal = (x*x) -(y*y) +c_re;
+                        y = 2 * x * y + c_im;
+                        x = x_temporal;
+                        iteraciones++;
+                    }
+                    if (iteraciones < 1000) 
+                    {
+                        bmp.SetPixel(col, row, Color.FromArgb(iteraciones % 128, iteraciones % 50 * 5, iteraciones % 10));
+                        
+                    } 
+                    else
+                    {
+                        bmp.SetPixel(col, row, Color.Black);
+                    }
+                }
+            }
+            ptbMandelbrot.Image = bmp;  
+        }
+
+        private void SierpinskiTriangle()
+        {
+            int width = ptbMandelbrot.Width;
+            int height = ptbMandelbrot.Height;
+            Bitmap bmp = new Bitmap(width, height);
+
+            using (Graphics g = Graphics.FromImage(bmp))
+            {
+                g.Clear(Color.White); // Fondo blanco
+                PointF p1 = new PointF(width / 2, 0);
+                PointF p2 = new PointF(0, height);
+                PointF p3 = new PointF(width, height);
+
+                DrawSierpinski(g, p1, p2, p3, 6); // Nivel de recursión
+            }
+
+            ptbMandelbrot.Image = bmp;
+        }
+
+        private void DrawSierpinski(Graphics g, PointF p1, PointF p2, PointF p3, int depth)
+        {
+            if (depth == 0)
+            {
+                PointF[] points = { p1, p2, p3 };
+                g.FillPolygon(Brushes.Black, points);
+            }
+            else
+            {
+                PointF mid1 = MidPoint(p1, p2);
+                PointF mid2 = MidPoint(p2, p3);
+                PointF mid3 = MidPoint(p3, p1);
+
+                DrawSierpinski(g, p1, mid1, mid3, depth - 1);
+                DrawSierpinski(g, mid1, p2, mid2, depth - 1);
+                DrawSierpinski(g, mid3, mid2, p3, depth - 1);
+            }
+        }
+
+        private PointF MidPoint(PointF p1, PointF p2)
+        {
+            return new PointF((p1.X + p2.X) / 2, (p1.Y + p2.Y) / 2);
+        }
+
+
+        private void KochSnowflake()
+        {
+            int width = ptbMandelbrot.Width;
+            int height = ptbMandelbrot.Height;
+            Bitmap bmp = new Bitmap(width, height);
+
+            using (Graphics g = Graphics.FromImage(bmp))
+            {
+                g.Clear(Color.White); // Fondo blanco
+
+                // Definir los tres vértices iniciales del triángulo equilátero
+                PointF p1 = new PointF(width / 4, height * 3 / 4);
+                PointF p2 = new PointF(width * 3 / 4, height * 3 / 4);
+                PointF p3 = new PointF(width / 2, height / 4);
+
+                DrawKochCurve(g, p1, p2, 5); // Lado 1
+                DrawKochCurve(g, p2, p3, 5); // Lado 2
+                DrawKochCurve(g, p3, p1, 5); // Lado 3
+            }
+
+            ptbMandelbrot.Image = bmp;
+        }
+
+        private void DrawKochCurve(Graphics g, PointF p1, PointF p2, int depth)
+        {
+            if (depth == 0)
+            {
+                g.DrawLine(Pens.Brown, p1, p2); // Dibuja línea base
+            }
+            else
+            {
+                // Calcula los puntos intermedios
+                PointF a = new PointF((2 * p1.X + p2.X) / 3, (2 * p1.Y + p2.Y) / 3);
+                PointF c = new PointF((p1.X + 2 * p2.X) / 3, (p1.Y + 2 * p2.Y) / 3);
+
+                // Rotar 60° para formar el "pico"
+                double angle = Math.PI / 3;
+                float bx = (float)(a.X + (c.X - a.X) * Math.Cos(angle) - (c.Y - a.Y) * Math.Sin(angle));
+                float by = (float)(a.Y + (c.X - a.X) * Math.Sin(angle) + (c.Y - a.Y) * Math.Cos(angle));
+                PointF b = new PointF(bx, by);
+
+                // Llamadas recursivas
+                DrawKochCurve(g, p1, a, depth - 1);
+                DrawKochCurve(g, a, b, depth - 1);
+                DrawKochCurve(g, b, c, depth - 1);
+                DrawKochCurve(g, c, p2, depth - 1);
+            }
+        }
+
+
+        private void DrawSpirograph()
+        {
+            int width = ptbMandelbrot.Width;
+            int height = ptbMandelbrot.Height;
+            Bitmap bmp = new Bitmap(width, height);
+
+            using (Graphics g = Graphics.FromImage(bmp))
+            {
+                g.Clear(Color.Black); // Fondo negro
+
+                // Dibujar múltiples patrones para mayor densidad
+                DrawSpirographPattern(g, width / 2, height / 2, 140, 60, 20, 5000, 0.05);
+                DrawSpirographPattern(g, width / 2, height / 2, 130, 55, 25, 5000, 0.05);
+                DrawSpirographPattern(g, width / 2, height / 2, 120, 50, 30, 5000, 0.05);
+            }
+
+            ptbMandelbrot.Image = bmp;
+        }
+
+        private void DrawSpirographPattern(Graphics g, int centerX, int centerY, int R, int r, int d, int points, double step)
+        {
+            List<PointF> pointsList = new List<PointF>();
+
+            for (int i = 0; i < points; i++)
+            {
+                double t = i * step;
+                double x = (R - r) * Math.Cos(t) + d * Math.Cos((R - r) * t / r);
+                double y = (R - r) * Math.Sin(t) - d * Math.Sin((R - r) * t / r);
+
+                pointsList.Add(new PointF((float)(centerX + x), (float)(centerY + y)));
+            }
+
+            // Dibujar líneas con colores degradados
+            for (int i = 1; i < pointsList.Count; i++)
+            {
+                Pen pen = new Pen(ColorFromGradient(i, pointsList.Count), 1);
+                g.DrawLine(pen, pointsList[i - 1], pointsList[i]);
+            }
+        }
+
+        private Color ColorFromGradient(int step, int maxSteps)
+        {
+            int r = (int)(255 * (Math.Sin(0.1 * step) * 0.5 + 0.5));
+            int g = (int)(255 * (Math.Sin(0.1 * step + 2) * 0.5 + 0.5));
+            int b = (int)(255 * (Math.Sin(0.1 * step + 4) * 0.5 + 0.5));
+            return Color.FromArgb(r, g, b);
+        }
+
+
+    }
+}
---
+++ FormsMenu/frmMenuPrincipal.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace FormsMenu
+{
+    partial class frmMenuPrincipal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            Menu1 = new Button();
+            Fractales = new Button();
+            Graficos = new Button();
+            Matrices = new Button();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.Transparent;
+            label1.Font = new Font("Yu Gothic", 25.8000011F, FontStyle.Bold | FontStyle.Underline, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.White;
+            label1.Location = new Point(12, 76);
+            label1.Name = "label1";
+            label1.Size = new Size(416, 56);
+            label1.TabIndex = 0;
+            label1.Text = "MENU PRINCIPAL";
+            // 
+            // Menu1
+            // 
+            Menu1.FlatAppearance.BorderSize = 0;
+            Menu1.Location = new Point(47, 241);
+            Menu1.Name = "Menu1";
+            Menu1.Size = new Size(94, 48);
+            Menu1.TabIndex = 1;
+            Menu1.Text = "Aceptar";
+            Menu1.UseVisualStyleBackColor = true;
+            // 
+            // Fractales
+            // 
+            Fractales.Location = new Point(47, 576);
+            Fractales.Name = "Fractales";
+            Fractales.Size = new Size(94, 50);
+            Fractales.TabIndex = 2;
+            Fractales.Text = "Aceptar";
+            Fractales.UseVisualStyleBackColor = true;
+            // 
+            // Graficos
+            // 
+            Graficos.Location = new Point(47, 338);
+            Graficos.Name = "Graficos";
+            Graficos.Size = new Size(94, 57);
+            Graficos.TabIndex = 3;
+            Graficos.Text = "Aceptar";
+            Graficos.UseVisualStyleBackColor = true;
+            // 
+            // Matrices
+            // 
+            Matrices.Location = new Point(47, 451);
+            Matrices.Name = "Matrices";
+            Matrices.Size = new Size(94, 59);
+            Matrices.TabIndex = 4;
+            Matrices.Text = "Aceptar";
+            Matrices.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(47, 218);
+            label2.Name = "label2";
+            label2.Size = new Size(61, 20);
+            label2.TabIndex = 5;
+            label2.Text = "Menu 1:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(47, 306);
+            label3.Name = "label3";
+            label3.Size = new Size(70, 20);
+            label3.TabIndex = 6;
+            label3.Text = "Graficos: ";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(47, 428);
+            label4.Name = "label4";
+            label4.Size = new Size(65, 20);
+            label4.TabIndex = 7;
+            label4.Text = "Matrices";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(49, 556);
+            label5.Name = "label5";
+            label5.Size = new Size(67, 20);
+            label5.TabIndex = 8;
+            label5.Text = "Fractales";
+            // 
+            // frmMenuPrincipal
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = Properties.Resources.fon_aplica;
+            BackgroundImageLayout = ImageLayout.Stretch;
+            ClientSize = new Size(438, 706);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(Matrices);
+            Controls.Add(Graficos);
+            Controls.Add(Fractales);
+            Controls.Add(Menu1);
+            Controls.Add(label1);
+            Name = "frmMenuPrincipal";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "frmMenuPrincipal";
+            Load += frmMenuPrincipal_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Button Menu1;
+        private Button Fractales;
+        private Button Graficos;
+        private Button Matrices;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+    }
+}
